8725f61c1bc87cc414c870270145526c2802042d
unknown <me2682@columbia.edu>
2020-05-15 12:14:00 +0300

    added the templates for two other pages

--- app.py
+++ app.py
@@ -9,9 +9,15 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 #static route
-@app.route("/1006")
+@app.route("/")
 def hello():
     return render_template('index.html')
+@app.route("/page2")
+def page2():
+    return render_template('page2.html')
+@app.route("/page3")
+def page3():
+    return render_template('page3.html')
 
 #start the server
 if __name__ == "__main__":
